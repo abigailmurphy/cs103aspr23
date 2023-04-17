384c831a08792799d6ed9b4ec35801bf1aaf427a
abigailmurphy <abigailmurphy@brandeis.edu>
2023-04-17 00:09:36 -0400

    Update transactions.js

--- lesson30/firstapp/routes/transactions.js
+++ lesson30/firstapp/routes/transactions.js
@@ -20,6 +20,24 @@ isLoggedIn = (req,res,next) => {
   }
 }
 
+router.get('/todo/',
+  isLoggedIn,
+  async (req, res, next) => {
+      const show = req.query.show
+      const completed = show=='completed'
+      let items=[]
+      if (show) { // show is completed or todo, so just show some items
+        items = 
+          await ToDoItem.find({userId:req.user._id, completed})
+                        .sort({completed:1,priority:1,createdAt:1})
+      }else {  // show is null, so show all of the items
+        items = 
+          await ToDoItem.find({userId:req.user._id})
+                        .sort({completed:1,priority:1,createdAt:1})
+
+      }
+            res.render('toDoList',{items,show,completed});
+});
 
 
 
